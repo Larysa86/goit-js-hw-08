3d4cf263cb01eeb8eec05463584da72affe86f02
Larysa Holtseva <holtseva@seznam.cz>
2022-10-11 11:06:55 +0200

    add gallery-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,50 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+
 const gallaryContainer = document.querySelector(".gallery");
 const gallaryItemsMarkup = createGallaryMarkup(galleryItems);
 
 gallaryContainer.innerHTML = gallaryItemsMarkup;
 
+var lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250});
+
 function createGallaryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-    <div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    /></a></div>`;
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
     })
     .join("");
 }
 
-gallaryContainer.addEventListener("click", onGallaryImageElClick);
-
-let instance = "";
-
-function onGallaryImageElClick(event) {
-  event.preventDefault();
-
-  const isGallaryImageEl = event.target.classList.contains("gallery__image");
-
-  if (!isGallaryImageEl) {
-    return;
-  }
-
-  instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`);
-  instance.show();
-  window.addEventListener("keydown", closeModal);
-}
-
-function closeModal(event) {
-    if (event.code === "Escape") {
-      instance.close();
-      window.removeEventListener("keydown", closeModal);
-    }
-  }
-
-console.log(galleryItems);
